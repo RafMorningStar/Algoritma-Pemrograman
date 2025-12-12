89a0e22fd633f62548c13208677c23602bec8677
RafMorningStar <rafiputraeffendi3@gmail.com>
2025-12-12 11:04:25 +0700

    Exercise paduka

--- Week-14/Latihan/Vokal.go
+++ Week-14/Latihan/Vokal.go
@@ -1,17 +1,21 @@
 package main
+
 import "fmt"
-func main(){
+
+func main() {
 	var total1, total2 int
 	var kar rune
-	
+
 	fmt.Scanf("%c", &kar)
-	
+
 	for kar != '.' {
-		if kar == 'A' && kar == 'a' && kar == 'I' && kar == 'i' && kar == 'U' && kar == 'u' && kar == 'E' && kar == 'e' && kar == 'O' && kar == 'o'{
+		if kar == 'A' || kar == 'a' || kar == 'I' || kar == 'i' || kar == 'U' || kar == 'u' ||
+			kar == 'E' || kar == 'e' || kar == 'O' || kar == 'o' {
 			total1 = total1 + 1
-		}else if kar != '.'{
+		} else if kar >= 'a' && kar <= 'z' || kar >= 'A' && kar <= 'Z' {
 			total2 = total2 + 1
-		} 
+		}
+		fmt.Scanf("%c", &kar)
 	}
 	fmt.Printf("Banyaknya huruf vokal: %v\nBanyaknya huruf konsonan: %v", total1, total2)
 }
--- Week-14/Latihan/palindrom.go
+++ Week-14/Latihan/palindrom.go
@@ -1,1 +1,22 @@
+package main
 
+import "fmt"
+
+func main() {
+	var kata string
+	var palindrom bool = true
+	var i, j int
+
+	fmt.Scan(&kata)
+
+	// i bergerak dari depan, j bergerak dari belakang
+	// Loop berjalan selama i masih di sebelah kiri j
+	for i, j = 0, len(kata)-1; i < j; i, j = i+1, j-1 {
+		if kata[i] != kata[j] {
+			palindrom = false
+			break // Jika ada satu saja yang beda, langsung berhenti
+		}
+	}
+
+	fmt.Println(palindrom)
+}
